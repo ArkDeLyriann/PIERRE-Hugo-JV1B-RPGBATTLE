7bc640d756a6a6521ee06961b45ca6e8e8265390
TheoChauveau53 <118454473+TheoChauveau53@users.noreply.github.com>
2022-11-24 15:50:26 +0100

    barre de vie du poulpe

--- JV1B_PIERRE_Hugo_RPGBATTLE_SCRIPT.js
+++ JV1B_PIERRE_Hugo_RPGBATTLE_SCRIPT.js
@@ -1,9 +1,12 @@
 atkDidier = document.getElementById("atkDidier");
-pvPoulpe = document.getElementById("pvPoulpe");
-afficheAction = document.getElementById("afficheAction");
+pvPoulpe = document.getElementById("pvPoulpe1");
+afficheAction = document.getElementById("afficheAction1");
+document.getElementById("pvPoulpe1").style.position = "relative"
+let health = document.getElementById("pvPoulpe1")
 
 atkDidier.onclick = function() {
-    pvPoulpe.innerHTML = parseInt(pvPoulpe.innerHTML)-10;
+    health.value -= 10;
+
     afficheAction.innerHTML = "J'ai fait 10 points de dégats.";
 }
 // Exercice : débuggez ce script :)
